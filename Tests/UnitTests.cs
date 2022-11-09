a4941ce1d5e6fb06b6ad196fce1e2b84b0bc45f0
tturgeon4 <89560389+tturgeon4@users.noreply.github.com>
2022-11-09 17:52:04 -0600

    Added last name to test functions

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_Turgeon()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_Invalid_Turgeon()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_Null_Turgeon()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,7 +31,7 @@ namespace GithubActionsLab
         }
         
         [Test]
-        public void Subtract_Valid()
+        public void Subtract_Valid_Turgeon()
         {
             Assert.AreEqual(5, Program.Subtract("6", "1"));
             Assert.AreEqual(3, Program.Subtract("7", "4"));
@@ -39,7 +39,7 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_Invalid_Turgeon()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
@@ -47,15 +47,15 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Subtract_Null()
+        public void Subtract_Null_Turgeon()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
         }
 		
-		[Test]
-        public void Multiply_Valid()
+	[Test]
+        public void Multiply_Valid_Turgeon()
         {
             Assert.AreEqual(20, Program.Multiply("10", "2"));
             Assert.AreEqual(16, Program.Multiply("4", "4"));
@@ -63,7 +63,7 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Multiply_Invalid()
+        public void Multiply_Invalid_Turgeon()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
@@ -71,15 +71,15 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Multiply_Null()
+        public void Multiply_Null_Turgeon()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
         }
 		
-		[Test]
-        public void Divide_Valid()
+	[Test]
+        public void Divide_Valid_Turgeon()
         {
             Assert.AreEqual(3, Program.Divide("3", "1"));
             Assert.AreEqual(1, Program.Divide("7", "7"));
@@ -87,7 +87,7 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Divide_Invalid()
+        public void Divide_Invalid_Turgeon()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
@@ -95,15 +95,15 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Divide_Null()
+        public void Divide_Null_Turgeon()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
         }
 		
-		[Test]
-        public void Power_Valid()
+	[Test]
+        public void Power_Valid_Turgeon()
         {
             Assert.AreEqual(216, Program.Power("6", "3"));
             Assert.AreEqual(1, Program.Power("1", "2"));
@@ -111,7 +111,7 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Power_Invalid()
+        public void Power_Invalid_Turgeon()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
             Assert.Throws<FormatException>(() => Program.Power("a", "1"));
@@ -119,7 +119,7 @@ namespace GithubActionsLab
         }
                             
         [Test]
-        public void Power_Null()
+        public void Power_Null_Turgeon()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
